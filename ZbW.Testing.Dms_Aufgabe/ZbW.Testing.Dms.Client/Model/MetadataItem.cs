d3c7e606b27e14fc6cdeda7539f0b7226cd18980
Gabriel Weibel <gabriel.weibel@hotmail.de>
2019-07-11 21:08:12 +0200

    ModelTests

--- ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client/Model/MetadataItem.cs
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.Client/Model/MetadataItem.cs
@@ -1,7 +1,31 @@
-﻿namespace ZbW.Testing.Dms.Client.Model
+﻿using System;
+using static System.String;
+
+namespace ZbW.Testing.Dms.Client.Model
 {
     public class MetadataItem
     {
-        // TODO: Write your Metadata properties here
+        public string Stichwoerter { get; set; } = Empty;
+        public string Bezeichnung { get; set; }
+        public string Benutzer { get; set; }
+        public DateTimeOffset Erfassungsdatum { get; set; }
+        public string FilePath { get; set; }
+        public bool IsRemoveFileEnabled { get; set; }
+        public string Type { get; set; }
+        public DateTimeOffset ValutaDatum { get; set; }
+        public MetadataItem()
+        {
+        }
+        public MetadataItem(string bezeichnung, DateTimeOffset erfassungsdatum, string filePath, bool isRemoveFileEnabled, string type, string stichwoerter, DateTimeOffset valutaDatum, string benutzer)
+        {
+            Bezeichnung = bezeichnung;
+            Benutzer = benutzer;
+            Erfassungsdatum = erfassungsdatum;
+            FilePath = filePath;
+            IsRemoveFileEnabled = isRemoveFileEnabled;
+            Type = type;
+            Stichwoerter = stichwoerter;
+            ValutaDatum = valutaDatum;
+        }
     }
 }
---
+++ ZbW.Testing.Dms_Aufgabe/ZbW.Testing.Dms.UnitTests/ModelTests/MetadataItemTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using NUnit.Framework;
+using ZbW.Testing.Dms.Client.Model;
+
+namespace ZbW.Testing.Dms.UnitTests.ModelTests
+{
+    [TestFixture]
+    internal class MetadataItemTests
+    {
+        private const string TestingText = "Test Something";
+        private readonly DateTimeOffset _testDateTime = DateTime.Now;
+        [Test]
+        public void MetadataItem_Bezeichnung_GetSetTestingText()
+        {
+            // Arrange
+            var metadataItem = new MetadataItem {Bezeichnung = TestingText};
+
+            // Act
+            var name = metadataItem.Bezeichnung;
+
+            // Assert
+            Assert.That(name, Is.EqualTo(TestingText));
+        }
+        [Test]
+        public void MetadataItem_Benutzer_GetSetTestingText()
+        {
+            // Arrange
+            var metadataItem = new MetadataItem {Benutzer = TestingText};
+
+            // Act
+            var username = metadataItem.Benutzer;
+
+            // Arrange
+            Assert.That(username, Is.EqualTo(TestingText));
+        }
+        [Test]
+        public void MetadataItem_FilePath_GetSetTestingText()
+        {
+            // Arrange
+            var metadataItem = new MetadataItem {FilePath = TestingText};
+
+            // Act
+            var filePath = metadataItem.FilePath;
+
+            // Assert
+            Assert.That(filePath, Is.EqualTo(TestingText));
+        }
+        [Test]
+        public void MetadataItem_SetIsRemoveFileEnabled_GetTrue()
+        {
+            // Arrange
+            var metadataItem = new MetadataItem {IsRemoveFileEnabled = true};
+
+            // Act
+            var isRemoveEnabled = metadataItem.IsRemoveFileEnabled;
+
+            // Assert
+            Assert.That(isRemoveEnabled, Is.True);
+        }
+        [Test]
+        public void MetadataItem_SetType_GetTestingText()
+        {
+            // Arrange
+            var metadataItem = new MetadataItem {Type = TestingText};
+
+            // Act
+            var type = metadataItem.Type;
+
+            // Assert
+            Assert.That(type, Is.EqualTo(TestingText));
+        }
+        [Test]
+        public void MetadateItem_Erfassungsdatum_GetTestDateTime()
+        {
+            // Arrange
+            var metadataItem = new MetadataItem {Erfassungsdatum = _testDateTime};
+
+            // Act
+            var result = metadataItem.Erfassungsdatum;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(_testDateTime));
+        }
+        [Test]
+        public void MetadataItem_SetValutaDatum_GetTestDateTime()
+        {
+            // Arrange
+            var metadataItem = new MetadataItem {ValutaDatum = _testDateTime};
+
+            // Act
+            var result = metadataItem.ValutaDatum;
+
+            // Assert
+            Assert.That(result, Is.EqualTo(_testDateTime));
+        }
+        [Test]
+        public void MetadataItem_SetStichwoerter_GetTestingText()
+        {
+            // Arrange
+            var metadataItem = new MetadataItem {Stichwoerter = TestingText};
+
+            // Act
+            var keyword = metadataItem.Stichwoerter;
+
+            // Assert
+            Assert.That(keyword, Is.EqualTo(TestingText));
+        }
+        [Test]
+        public void MetadataItem_FullConstructor_GetTestingTextsAndDates()
+        {
+            // Arrange and Act
+            var metadataItem = new MetadataItem(TestingText, _testDateTime, TestingText, false, TestingText, TestingText, _testDateTime, TestingText);
+
+            // Assert
+            Assert.That(metadataItem.Benutzer, Is.EqualTo(TestingText));
+            Assert.That(metadataItem.Bezeichnung, Is.EqualTo(TestingText));
+            Assert.That(metadataItem.Erfassungsdatum, Is.EqualTo(_testDateTime));
+            Assert.That(metadataItem.FilePath, Is.EqualTo(TestingText));
+            Assert.That(metadataItem.IsRemoveFileEnabled, Is.False);
+            Assert.That(metadataItem.Stichwoerter, Is.EqualTo(TestingText));
+            Assert.That(metadataItem.Type, Is.EqualTo(TestingText));
+            Assert.That(metadataItem.ValutaDatum, Is.EqualTo(_testDateTime));
+        }
+    }
+}
